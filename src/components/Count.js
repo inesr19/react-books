a97a51540f94c3b041eee352c294d1168f5ac973
Ines Radic <inesradic12314@gmail.com>
2021-03-28 18:14:35 -0700

    add text and button

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,22 +1,31 @@
 import React, {useState, useEffect} from "react";
+import "../styles/Count.css";
 
 function Count() { 
-    const [counter, setCounter] = useState(10);
+    const [counter, setCounter] = useState(5);
       
-useEffect(() => {
-    const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-
+    useEffect(() => {
+        const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    
     return () => clearInterval(timer);
     
 }, [counter]);
 
 if(counter === 0) {
-    return <h1 className="hello">Hello World</h1>
+    return (
+    <div>
+    <h1 className="hello">Hello World</h1>
+    <p className="align">Click the button below to learn more.
+    <hr />
+    <button>Click me!</button>
+    </p>
+    </div>
+    )
 }
 
 return (
     <div>
-        <div className="countdown">Countdown: {counter}</div>
+        <div className="countdown">{counter}</div>
     </div>
         );
 }
