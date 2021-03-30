38b4e6b61d4cbbd5e0106204f580789fa8e5d555
Ines Radic <inesradic12314@gmail.com>
2021-03-29 21:15:16 -0700

    add home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect} from "react";
 
-class Home extends Component {
-    render() {
-        return (
-            <div>Hello World!</div>
-        )
-    }
+function Home() {
+    const [firstName, setName] = useState({
+        name: ""
+    })
+    
+    const [favColor, setColor] = useState({
+        color: ""
+    })
+
+
+    return (
+        
+    <div className="card">
+      <div>
+        Name: {firstName.name}
+      </div>
+      <div>
+        Favorite color: {favColor.color}
+      </div>
+      <div>
+        <button onClick={() => setName({ ...firstName, name: "harry" })} className="btn">
+          Click for name
+        </button>
+        <button onClick={() => setColor({ ...favColor, color: "Red" })} className="btn">
+          Click for favorite color
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
